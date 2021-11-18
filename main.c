ec77a5cec17a9cb15d6e23ecf8efc54212d5d5ac
Abdoulaye Dia <abdoulaye@jgandco.consulting>
2021-11-18 01:02:43 +0100

    Queue implémentation, with expansion of size

--- main.c
+++ main.c
@@ -2,35 +2,43 @@
 
 int main(int argc, char const *argv[])
 {
-
+    
     srand(time(NULL));
-
+    
     queue *q = (queue *)(malloc(sizeof(queue)));
-
+    
     createEmptyQueue(q);
-
+    
     enqueue(q, rand() % 100);
     enqueue(q, rand() % 100);
     enqueue(q, rand() % 100);
-
+    
     print_queue(q);
-
+    
     printf("front: %d\n", peek(q));
-    printf("read: %d\n", rear(q));
-
-    printf("dequeu: %d\n", dequeue(q));
-    printf("dequeu: %d\n", dequeue(q));
-
-    printf("front: %d\n", peek(q));
-
-    clear(q);
-
-    enqueue(q, rand() % 100);
-    print_queue(q);
-
-    printf("front: %d\n", peek(q));
-    printf("read: %d\n", rear(q));
-
+    printf("rear: %d\n\n", rear(q));
+    
+    printf("dequeu: %d size: %d\n", dequeue(q), size(q));
+    printf("dequeu: %d size: %d\n", dequeue(q), size(q));
+    printf("dequeu: %d size: %d\n\n", dequeue(q), size(q));
+    
+    
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    enqueue(q, rand()%100);
+    
+    printf("Queue full: %s", is_full(q) ? "true" : "false");
+    
+    enqueue(q, rand()%100);
+    
+    printf("Queue full: %s", is_full(q) ? "true" : "false");
+    
     clean_queue(q);
     return 0;
 }
--- queue.h
+++ queue.h
@@ -14,10 +14,11 @@ int is_full(queue *q);
 int is_empty(queue *q);
 int peek(queue *q);
 int rear(queue *q);
+int size(const queue* q);
 
 int copy(int *arr, int *data, int size);
 int expand(queue *q);
-int clear(queue *q);
+int clear_queue(queue *q);
 
 void clean_queue(queue *q);
 void print_queue(queue *q);
@@ -33,46 +34,45 @@ int createEmptyQueue(queue *q)
 {
     if (q == NULL)
         return -1;
-
+    
     q->elements = (int *)(malloc(sizeof(int) * DEFAULT_SIZE));
     q->front = q->rear = -1;
     q->size = 0;
     q->max = DEFAULT_SIZE;
+    
+    return 1;
 }
 
 int enqueue(queue *q, int val)
 {
     if (q != NULL)
     {
-        if (q->size < q->max)
-        {
-            q->size++;
+        q->rear++;
+        q->size++;
+        
+        if(q->front == -1)
             q->front++;
-            if (q->size == 1)
-            {
-                q->rear = q->front;
-            }
-            else if (q->size == q->max)
-            {
-                expand(q);
-            }
-            q->elements[q->front] = val;
-            return 1;
-        }
+        
+        if(q->size > q->max)
+            expand(q);
+        
+        q->elements[q->rear] = val;
+        
+        return 1;
     }
     return -1;
 }
 
 int dequeue(queue *q)
 {
-    if (q == NULL || q->size == 0)
-        return -1;
-
-    q->front--;
-    if (q->front == -1)
-        q->rear == -1;
-
-    return q->elements[q->front];
+    if (q != NULL ){
+        if(q->size > 1){
+            q->size--;
+            return q->elements[++q->front];
+        }
+        clear_queue(q);
+    }
+    return -1;
 }
 
 int is_full(queue *q)
@@ -89,7 +89,7 @@ int peek(queue *q)
 {
     if (q == NULL || q->front == -1)
         return -1;
-
+    
     return q->elements[q->front];
 }
 
@@ -97,16 +97,24 @@ int rear(queue *q)
 {
     if (q == NULL || q->rear == -1)
         return -1;
-
+    
     return q->elements[q->rear];
+}
+
+int size(const queue* q)
+{
+    if(q != NULL)
+        return q->size;
+    
+    return -1;
 }
 
 int copy(int *arr, int *data, int size)
 {
-
+    
     if (arr == NULL || data == NULL || size < 0)
         return -1;
-
+    
     for (size_t i = 0; i < size; i++)
     {
         *(arr + i) = *(data + i);
@@ -118,33 +126,35 @@ int expand(queue *q)
 {
     if (q == NULL)
         return -1;
-
+    
     int *oldElements = q->elements;
     int newMax = q->max * 2;
-
+    
     q->elements = (int *)(malloc(sizeof(int) * newMax));
-
+    
     copy(q->elements, oldElements, q->max);
-
+    
     q->max = newMax;
-
+    
     free(oldElements);
     return 1;
 }
 
-int clear(queue *q)
+int clear_queue(queue *q)
 {
-    if (q == NULL || q->size == 0)
+    if (q == NULL)
         return -1;
-
+    
     q->size = 0;
     q->front = q->rear = -1;
+    return 1;
 }
 
 void clean_queue(queue *q)
 {
     free(q->elements);
     free(q);
+
 }
 void print_queue(queue *q)
 {
@@ -153,7 +163,7 @@ void print_queue(queue *q)
         printf("Invalid queue");
         return;
     }
-
+    
     if (is_empty(q))
     {
         printf("Empty queue");
@@ -161,7 +171,7 @@ void print_queue(queue *q)
     else
     {
         printf("\nmax: %d\nsize: %d\nfront: %d, rear: %d\n\n", q->max, q->size, q->front, q->rear);
-        for (size_t i = 0; i < q->size; i++)
+        for (int i = q->front; i < q->size; i++)
         {
             printf("%d : %d\n", i, *(q->elements + i));
         }
